534b96961305ab41eef0a91c06f4402ebeb9ba14
PycJIaHK0 <dubchakruslan@gmail.com>
2014-12-02 14:08:25 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<tr id="header">
+<td>
+
+<div id="logo" class="png">
+		<a href="http://www.tneu.edu.ua/" target="_blank" title="Головна сторінка"> <img src="http://rozklad.loc/images/logo.png" 
+		alt="Логотип"     style="margin-bottom: 40px;"> </a>
+	</div>
+	<div id="h_img" class="png" >
+	</div>
+	
+	
+	
+</td>
+</tr>
