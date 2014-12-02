d2276c9f06c40c9596a309d355d7e922e687e98c
PycJIaHK0 <dubchakruslan@gmail.com>
2014-12-02 14:02:02 +0200

    Create authorization.php

---
+++ authorization.php
@@ -0,0 +1,6 @@
+<form id="enter" method="POST" action="index.php">
+
+ <input type="text" id="e_log" name="e_login" style="(../css/style.css)" placeholder=" Логін" required/><br>
+ <input type="password" id="e_pass" name="e_password" style="(../css/style.css)" placeholder=" Пароль" required/><br>
+ <input type="submit" id="e_button" value="" name="enter" ;/>
+ </form> 
